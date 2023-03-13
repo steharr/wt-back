20dc179ae3aa6399fc487b1722f5cf9b7b3c1534
steharr <steharrold@gmail.com>
2023-03-13 14:31:46 +0000

    setup db connection with environment profiles

---
+++ src/main/java/com/workout/config/dev/DataSourceConfig.java
@@ -0,0 +1,37 @@
+package com.workout.config.dev;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.*;
+
+import javax.sql.DataSource;
+
+@Profile("DEV")
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:dev.properties"),
+})
+public class DataSourceConfig {
+
+    @Value("${db.driver}")
+    private String driver;
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.user}")
+    private String user;
+    @Value("${db.pw}")
+    private String pw;
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(url);
+        dataSourceBuilder.username(user);
+        dataSourceBuilder.password(pw);
+        return dataSourceBuilder.build();
+    }
+}
+
+
