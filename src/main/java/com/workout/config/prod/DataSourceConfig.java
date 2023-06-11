26e2c6668ce8db199218a65468f55ef9e54c135d
steharr <steharrold@gmail.com>
2023-06-11 23:28:58 +0100

    enhance setup for prod

--- src/main/java/com/workout/config/prod/DataSourceConfig.java
+++ src/main/java/com/workout/config/prod/DataSourceConfig.java
@@ -1,35 +1,28 @@
 package com.workout.config.prod;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Profile("PROD")
 @Configuration
-@PropertySources({
-        @PropertySource("classpath:prod.properties"),
-})
 public class DataSourceConfig {
 
-    @Value("${db.driver}")
-    private String driver;
-    @Value("${db.url}")
-    private String url;
-
-    @Value("${db.user}")
-    private String user;
-    @Value("${db.pw}")
-    private String pw;
+    @Autowired
+    private Environment environment;
 
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName(driver);
-        dataSourceBuilder.url(url);
-        dataSourceBuilder.username(user);
-        dataSourceBuilder.password(pw);
+        dataSourceBuilder.driverClassName(environment.getProperty("db.driver"));
+        dataSourceBuilder.url(environment.getProperty("db.url"));
+        dataSourceBuilder.username(environment.getProperty("db.user"));
+        dataSourceBuilder.password(environment.getProperty("db.pw"));
         return dataSourceBuilder.build();
     }
 }
