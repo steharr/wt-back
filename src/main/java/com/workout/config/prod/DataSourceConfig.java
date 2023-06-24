a1d64f3d01d74c87a94390f7c4a4fc3e81613e1e
steharr <steharrold@gmail.com>
2023-06-25 00:24:13 +0100

    remove reliance on prod properties file

--- src/main/java/com/workout/config/prod/DataSourceConfig.java
+++ src/main/java/com/workout/config/prod/DataSourceConfig.java
@@ -2,30 +2,17 @@ package com.workout.config.prod;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Profile("PROD")
 @Configuration
-@PropertySources({
-        @PropertySource("classpath:prod.properties"),
-})
 public class DataSourceConfig {
-
-    @Value("${db.driver}")
-    private String driver;
-    @Value("${db.url}")
-    private String url;
-
-    @Value("${db.user}")
-    private String user;
-    @Value("${db.pw}")
-    private String pw;
-
     @Autowired
     private Environment environment;
 
