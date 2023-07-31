db669625150f951779f56ba0a4ce92b29e18eb87
steharr <steharrold@gmail.com>
2023-07-31 22:45:08 +0100

    fix delete cors error

--- src/main/java/com/workout/security/application/config/SecurityConfig.java
+++ src/main/java/com/workout/security/application/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -67,6 +68,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues()); // Apply the configuration to all paths
+        configuration.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT"));
         return source;
     }
 
